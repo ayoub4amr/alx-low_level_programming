f4d8fe97d984ea0dc431712fdd8e48de90742dcf
ayoub4amr <ayoub4amr@gmail.com>
2023-06-15 04:18:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * a program that prints exactly Programming is like building a multilingual puzzle
+ * main - Entry point
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
